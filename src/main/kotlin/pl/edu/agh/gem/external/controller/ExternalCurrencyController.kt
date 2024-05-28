83c05b41e0d7143eb7fa64705281775c22d99163
Jakub Spolnik <lawos231@gmail.com>
2024-05-28 21:52:21 +0200

    GEM-18 | Cr changes

--- src/main/kotlin/pl/edu/agh/gem/external/controller/ExternalCurrencyController.kt
+++ src/main/kotlin/pl/edu/agh/gem/external/controller/ExternalCurrencyController.kt
@@ -1,7 +1,9 @@
 package pl.edu.agh.gem.external.controller
 
+import org.springframework.http.HttpStatus.OK
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import pl.edu.agh.gem.external.dto.ExternalAvailableCurrenciesResponse
 import pl.edu.agh.gem.external.dto.toExternalAvailableCurrenciesResponse
@@ -16,6 +18,7 @@ class ExternalCurrencyController(
 ) {
 
     @GetMapping(produces = [APPLICATION_JSON_INTERNAL_VER_1])
+    @ResponseStatus(OK)
     fun getAvailableCurrencies(): ExternalAvailableCurrenciesResponse {
         return currencyService.getAvailableCurrencies().toExternalAvailableCurrenciesResponse()
     }
